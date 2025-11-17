aa88a8e6586acb70519470b8929a14e0688ee2eb
franky franco <frankyfranco456@gmail.com>
2025-11-16 21:12:57 -0500

    tarea exitosa

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { HttpModule } from '@nestjs/axios';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
 
 @Module({
   controllers: [SeedController],
-  imports: [HttpModule],
+  imports: [HttpModule, PokemonModule],
   providers: [SeedService],
   exports: [SeedService]
 })
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from "@nestjs/axios";
 import { lastValueFrom } from 'rxjs';
 import { PokeApiResponse } from '../seed/interfaces/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
 
-  constructor(private http: HttpService) {
+  constructor(private http: HttpService, private readonly pokemonService: PokemonService) {
   }
 
   async executeSeed() {
-    const response$ = await this.http.get <PokeApiResponse>('https://pokeapi.co/api/v2/pokemon?limit=100')
+    const response$ = await this.http.get <PokeApiResponse>('https://pokeapi.co/api/v2/pokemon?limit=1000')
     const {data} = await lastValueFrom(response$);
 
-    data.results.forEach(({name , url}) => {
+    data.results.forEach(async ({name , url}) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({name, no});
+      await this.pokemonService.create({name, no});
+      console.log(`Pokemon con el no ${no} y nombre ${name} creado exitosamente`);
     }
   );
     return data.results;
